5affefa9a1ac54176710286b7c5a4c91dfe00a98
Markus Grotz <grotz@uw.edu>
2022-09-08 21:38:54 -0700

    code format with black

--- click_prompt/__init__.py
+++ click_prompt/__init__.py
@@ -23,24 +23,29 @@ class ChoiceParameter(click.Parameter, ABC):
     [1] https://stackoverflow.com/questions/54311067/
     """
 
-    def __init__(self,
-                 param_decls: Optional[Sequence[str]] = None,
-                 prompt: Union[bool, str] = True,
-                 **kwargs):
+    def __init__(
+        self,
+        param_decls: Optional[Sequence[str]] = None,
+        prompt: Union[bool, str] = True,
+        **kwargs
+    ):
         super().__init__(param_decls, prompt=prompt, multiple=False, **kwargs)
         if not isinstance(self.type, click.Choice):
-            raise Exception('ChoiceOption type arg must be click.Choice')
+            raise Exception("ChoiceOption type arg must be click.Choice")
 
     def prompt_for_value(self, ctx: click.core.Context) -> Any:
         if len(self.type.choices) == 1:
             return self.type.choices[0]
         return questionary.select(self.prompt, choices=self.type.choices).unsafe_ask()
 
+
 class ChoiceOption(ChoiceParameter, click.Option):
     pass
 
+
 class ChoiceArgument(ChoiceParameter, click.Argument):
     pass
+
 
 class MultipleParameter(click.Parameter, ABC):
     """
@@ -49,24 +54,29 @@ class MultipleParameter(click.Parameter, ABC):
     contains a single item.
     """
 
-    def __init__(self,
-                 param_decls: Optional[Sequence[str]] = None,
-                 prompt: Union[bool, str] = True,
-                 **kwargs):
+    def __init__(
+        self,
+        param_decls: Optional[Sequence[str]] = None,
+        prompt: Union[bool, str] = True,
+        **kwargs
+    ):
         super().__init__(param_decls, prompt=prompt, multiple=True, **kwargs)
         if not isinstance(self.type, click.Choice):
-            raise Exception('MultipleOption type arg must be click.Choice')
+            raise Exception("MultipleOption type arg must be click.Choice")
 
     def prompt_for_value(self, ctx: click.core.Context) -> Any:
         if len(self.type.choices) == 1:
             return [self.type.choices[0]]
         return questionary.checkbox(self.prompt, choices=self.type.choices).unsafe_ask()
 
+
 class MultipleOption(MultipleParameter, click.Option):
     pass
 
+
 class MultipleArgument(MultipleParameter, click.Argument):
     pass
+
 
 class ConfirmParameter(click.Parameter, ABC):
     """
@@ -74,64 +84,80 @@ class ConfirmParameter(click.Parameter, ABC):
     onboard features.
     """
 
-    def __init__(self,
-                 param_decls: Optional[Sequence[str]] = None,
-                 prompt: Union[bool, str] = True,
-                 **kwargs):
+    def __init__(
+        self,
+        param_decls: Optional[Sequence[str]] = None,
+        prompt: Union[bool, str] = True,
+        **kwargs
+    ):
         super().__init__(param_decls, prompt=prompt, is_flag=True, **kwargs)
 
     def prompt_for_value(self, ctx: click.core.Context) -> Any:
         return questionary.confirm(self.prompt, default=self.default).unsafe_ask()
 
+
 class ConfirmOption(ConfirmParameter, click.Option):
     pass
 
+
 class ConfirmArgument(ConfirmParameter, click.Argument):
     pass
+
 
 class FilePathParameter(click.Parameter, ABC):
     """
     Allows the user to sepcify a path.
     """
 
-    def __init__(self,
-                 param_decls: Optional[Sequence[str]] = None,
-                 prompt: Union[bool, str] = True,
-                 **kwargs):
+    def __init__(
+        self,
+        param_decls: Optional[Sequence[str]] = None,
+        prompt: Union[bool, str] = True,
+        **kwargs
+    ):
         super().__init__(param_decls, prompt=prompt, **kwargs)
-        self.default = self.default or '~'
+        self.default = self.default or "~"
 
     def prompt_for_value(self, ctx: click.core.Context) -> Any:
         return questionary.path(self.prompt, default=self.default).unsafe_ask()
 
+
 class FilePathOption(FilePathParameter, click.Option):
     pass
 
+
 class FilePathArgument(FilePathParameter, click.Argument):
     pass
+
 
 class AutoCompleteParameter(click.Parameter, ABC):
     """
     Auto complete user input.
     """
 
-    def __init__(self,
-                 param_decls: Optional[Sequence[str]] = None,
-                 prompt: Union[bool, str] = True,
-                 choices=None,
-                 **kwargs):
+    def __init__(
+        self,
+        param_decls: Optional[Sequence[str]] = None,
+        prompt: Union[bool, str] = True,
+        choices=None,
+        **kwargs
+    ):
         super().__init__(param_decls, prompt=prompt, **kwargs)
         if isinstance(self.type, click.Choice):
             self.choices = self.type.choices
         else:
             self.choices = choices or []
-        self.default = self.default or ''
+        self.default = self.default or ""
 
     def prompt_for_value(self, ctx: click.core.Context) -> Any:
-        return questionary.autocomplete(self.prompt, self.choices, self.default).unsafe_ask()
+        return questionary.autocomplete(
+            self.prompt, self.choices, self.default
+        ).unsafe_ask()
+
 
 class AutoCompleteOption(AutoCompleteParameter, click.Option):
     pass
+
 
 class AutoCompleteArgument(AutoCompleteParameter, click.Argument):
     pass
--- example.py
+++ example.py
@@ -4,7 +4,8 @@
 from typing import Sequence
 
 import click
-#import rich_click as click
+
+# import rich_click as click
 
 from click_prompt import ChoiceOption
 from click_prompt import MultipleOption
@@ -12,8 +13,19 @@ from click_prompt import ConfirmOption
 from click_prompt import FilePathOption
 from click_prompt import AutoCompleteOption
 
-FRUITS = [ 'Apples', 'Bananas', 'Grapefruits', 'Mangoes', 'Oranges', 'Pears',
-           'Peaches', 'Raspberries', 'Strawberries', 'Watermelons' ]
+FRUITS = [
+    "Apples",
+    "Bananas",
+    "Grapefruits",
+    "Mangoes",
+    "Oranges",
+    "Pears",
+    "Peaches",
+    "Raspberries",
+    "Strawberries",
+    "Watermelons",
+]
+
 
 @click.group()
 def cli(**kwargs):
@@ -21,41 +33,61 @@ def cli(**kwargs):
 
 
 @cli.command()
-@click.option('--choice', prompt='What do you want to eat?', type=click.Choice(FRUITS), cls=ChoiceOption)
+@click.option(
+    "--choice",
+    prompt="What do you want to eat?",
+    type=click.Choice(FRUITS),
+    cls=ChoiceOption,
+)
 def single(choice: str):
     print(choice)
 
+
 @cli.command()
-@click.option('--options', prompt='Select smoothie ingredients', type=click.Choice(FRUITS), cls=MultipleOption)
+@click.option(
+    "--options",
+    prompt="Select smoothie ingredients",
+    type=click.Choice(FRUITS),
+    cls=MultipleOption,
+)
 def multiple(options: Sequence[str]):
     print(options)
 
+
 @cli.command()
-@click.option('--confirm', prompt='Do you really want to ?', cls=ConfirmOption)
+@click.option("--confirm", prompt="Do you really want to ?", cls=ConfirmOption)
 def confirm(confirm: bool):
     print(confirm)
 
+
 @cli.command()
-@click.option('--confirm', prompt='Confirm', is_flag=True)
+@click.option("--confirm", prompt="Confirm", is_flag=True)
 def confirm_click(confirm: bool):
     print(confirm)
 
+
 @cli.command()
-@click.option('--path', cls=FilePathOption)
+@click.option("--path", cls=FilePathOption)
 def file(path: str):
     print(path)
 
+
 @cli.command()
-@click.option('--complete', type=click.Choice(FRUITS), cls=AutoCompleteOption)
+@click.option("--complete", type=click.Choice(FRUITS), cls=AutoCompleteOption)
 def auto_choice(complete: str):
     print(complete)
 
+
 @cli.command()
-@click.option('--complete', prompt='What is your favourite fruit?', choices=FRUITS, cls=AutoCompleteOption)
+@click.option(
+    "--complete",
+    prompt="What is your favourite fruit?",
+    choices=FRUITS,
+    cls=AutoCompleteOption,
+)
 def auto(complete: str):
     print(complete)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
-
