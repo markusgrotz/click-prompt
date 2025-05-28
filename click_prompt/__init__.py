38dc16ba736f4d234020adb24149494b20738184
Markus Grotz <grotz@uw.edu>
2025-05-28 04:00:11 +0200

    skip checks for imports

--- click_prompt/__init__.py
+++ click_prompt/__init__.py
@@ -3,8 +3,8 @@ Provides additional interactive command line interface options for the
 click framework using the questionary library
 """
 
-from click_prompt.core.argument import *
-from click_prompt.core.option import *
+from click_prompt.core.argument import *  # noqa: F403
+from click_prompt.core.option import *  # noqa: F403
 
-from click_prompt.decorators.argument import *
-from click_prompt.decorators.option import *
+from click_prompt.decorators.argument import *  # noqa: F403
+from click_prompt.decorators.option import *  # noqa: F403
